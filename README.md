899b729ba3678123743a5f04e595c493e9912e8e
long.woo <longwunet@outlook.com>
2021-07-15 14:12:17 +0800

    release: v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stylelint-config-standard
+
+> 扩展 `stylelint-config-standard` 。
+
 Stylelint 自定义配置。
--- index.js
+++ index.js
@@ -7,12 +7,15 @@ module.exports = {
         baseIndentLevel: 0
       }
     ],
+    'color-named': 'never',
     'declaration-block-semicolon-newline-after': 'always',
     'selector-pseudo-element-no-unknown': [true, {
       ignorePseudoElements: ['v-deep']
     }],
     'at-rule-no-unknown': [true, {
       ignoreAtRules: ['mixin', 'include']
-    }]
+    }],
+    'block-opening-brace-newline-after': 'always',
+    'block-closing-brace-newline-before': 'always'
   }
 }
